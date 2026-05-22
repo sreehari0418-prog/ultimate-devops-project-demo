a0aead7b424bf12704d505f8c9ee15ac134653c1
Roli <roli@Rolis-MacBook-Air.local>
2026-05-22 17:17:42 +0530

    test: trigger CI workflow on githubcicheck branch

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 package main
 
+// Testing CI build workflow trigger.
+
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate protoc --go_out=./ --go-grpc_out=./ --proto_path=../../pb ../../pb/demo.proto
